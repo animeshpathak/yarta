761dcce363b6b71fae48e53c42253718bc5f43aa
georgerosca <georgerosca@645dabaa-2f37-4c9f-80e1-9e4206c85bdd>
2014-04-28 09:25:41 +0000

    task-id: 5745 task-url: https://gforge.inria.fr/pm/t_follow.php/5745 task-description: jena exception for the first time; increasing the buffer size solved the problem;

--- trunk/samples/android/INRIADonations/src/fr/inria/arles/giveaway/DonationApp.java
+++ trunk/samples/android/INRIADonations/src/fr/inria/arles/giveaway/DonationApp.java
@@ -136,7 +136,7 @@ public class DonationApp extends Application implements MSEApplication,
 					+ fileName);
 
 			int count = 0;
-			byte buffer[] = new byte[1024];
+			byte buffer[] = new byte[4096];
 
 			while ((count = fin.read(buffer)) != -1) {
 				fout.write(buffer, 0, count);
