6809e3c2984bf68976682b73c17fa5f52eec2cf2
georgerosca <grosca@users.noreply.github.com>
2014-09-06 08:45:49 +0000

    - added removeResource to mse knowledgebase; - added caching of data type properties;
    updated mse-1.2.rdf
    - added time, content and hasReply to Content to support sorting, content description and comments;
    - added hasContet to Group to support Group contents;
    
    updated donations app

--- samples/android/INRIADonations/src/fr/inria/arles/giveaway/resources/AnnouncementImpl.java
+++ samples/android/INRIADonations/src/fr/inria/arles/giveaway/resources/AnnouncementImpl.java
@@ -1,9 +1,14 @@
 package fr.inria.arles.giveaway.resources;
 
 import fr.inria.arles.yarta.middleware.msemanagement.ThinStorageAccessManager;
+import fr.inria.arles.yarta.resources.Content;
+import fr.inria.arles.yarta.resources.Group;
+import fr.inria.arles.yarta.resources.Message;
 import fr.inria.arles.yarta.resources.YartaResource;
 import fr.inria.arles.yarta.resources.Agent;
+
 import java.util.Set;
+
 import fr.inria.arles.yarta.knowledgebase.interfaces.Node;
 import fr.inria.arles.yarta.resources.Topic;
 
@@ -43,25 +48,6 @@ public class AnnouncementImpl extends YartaResource implements Announcement {
 	public void setTitle(String string) {
 		sam.setDataProperty(kbNode, PROPERTY_TITLE_URI, String.class,
 				string);
-	}
-
-	/**
-	 * @return the time. Null if value is not set.
-	 */
-	public Long getTime() {
-		return Long.valueOf(sam.getDataProperty(kbNode, PROPERTY_TIME_URI,
-				String.class));
-	}
-
-	/**
-	 * Sets the time.
-	 * 
-	 * @param	long
-	 *			the time to be set
-	 */
-	public void setTime(Long time) {
-		sam.setDataProperty(kbNode, PROPERTY_TIME_URI, String.class,
-				String.valueOf(time));
 	}
 
 	/**
@@ -317,5 +303,56 @@ public class AnnouncementImpl extends YartaResource implements Announcement {
 	@Override
 	public Set<Agent> getCreator_inverse() {
 		return sam.getObjectProperty_inverse(kbNode, Agent.PROPERTY_CREATOR_URI);
+	}
+
+	@Override
+	public Set<Group> getHasContent_inverse() {
+		return sam.getObjectProperty_inverse(kbNode,
+				Group.PROPERTY_HASCONTENT_URI);
+	}
+	
+	@Override
+	public boolean addHasReply(Content c) {
+		return sam.setObjectProperty(kbNode, PROPERTY_HASREPLY_URI, c);
+	}
+
+	@Override
+	public Set<Content> getHasReply() {
+		return sam.getObjectProperty(kbNode, PROPERTY_HASREPLY_URI);
+	}
+
+	@Override
+	public boolean deleteHasReply(Content c) {
+		return sam.deleteObjectProperty(kbNode, PROPERTY_HASREPLY_URI, c);
+	}
+	
+	@Override
+	public Set<Content> getHasReply_inverse() {
+		return sam.getObjectProperty_inverse(kbNode,
+				Content.PROPERTY_HASREPLY_URI);
+	}
+	
+	@Override
+	public Long getTime() {
+		return Long.valueOf(sam.getDataProperty(kbNode,
+				Message.PROPERTY_TIME_URI, String.class));
+	}
+
+	@Override
+	public void setTime(Long timestamp) {
+		sam.setDataProperty(kbNode, Message.PROPERTY_TIME_URI, String.class,
+				String.valueOf(timestamp));
+	}
+
+	@Override
+	public String getContent() {
+		return sam.getDataProperty(kbNode, Message.PROPERTY_CONTENT_URI,
+				String.class);
+	}
+
+	@Override
+	public void setContent(String content) {
+		sam.setDataProperty(kbNode, Message.PROPERTY_CONTENT_URI, String.class,
+				content);
 	}
 }
